f8652288833e3bc5ef2de4dba6b72477e92ef25e
ItsManikanta <maddukurimanikanta0@gmail.com>
2026-02-24 12:49:01 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,19 @@
-import Content from "./Content"
-import Footer from "./Footer"
-import Header from "./Header"
-import Highlights from "./Highlights"
-import Product from "./products"
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+import Highlights from "./Highlights";
+import Product from "./Products"; // ✅ Fixed case to match file name
 
-function Home(){
-  return(
+function Home() {
+  return (
     <>
-    <Header/>
-    <Content/>
-    <Highlights/>
-    <Footer/>
-    <Product/>
-    
+      <Header />
+      <Content />
+      <Highlights />
+      <Footer />
+      <Product />
     </>
-  )
+  );
 }
-export default Home
+
+export default Home;
